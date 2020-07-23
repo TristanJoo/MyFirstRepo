9b6d06b27adc25376d2574842586491bcf6f5276
TristanJoo <68696849+TristanJoo@users.noreply.github.com>
2020-07-23 10:13:33 -0400

    Added html, css & javascript

---
+++ script.js
@@ -0,0 +1,43 @@
+console.clear();
+
+console.log("Hello World");
+
+let myFirstVariable = 7;
+let mySecondVariable = 2;
+console.log(myFirstVariable + mySecondVariable);
+
+const firstName = "Tristan";
+const lastName = "Joo";
+
+console.log(firstName + lasName);
+
+console.log(firstName + " " + lastName);
+
+myFirstVariable = 1000;
+mySecondVariable = 20;
+
+console.log(myFirstVariable + mySecondVariable);
+
+console.log(myFirstVariable > mySecondVariable);
+
+function returnLargest(){
+    //function body//
+    if(myFirstVariable > mySecondVariable){
+        console.log(myFirstVariable);
+    } else{
+        console.log(mySecondVariable);
+    }
+}
+
+let title = document.querySelector('h1');
+title.textContext = "Ro";
+
+console.log(title);
+
+
+;et daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+console.log(daysOfWeek);
+console.log(daysOfWeek[0]);
+
+returnLargest();
